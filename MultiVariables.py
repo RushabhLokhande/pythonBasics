b60fc2cbbb30041ad4183222b03a9d7b69015f7f
RushabhLokhande <rushabhlokhande7171@gmail.com>
2021-04-23 12:08:01 -0700

    first commit

---
+++ MultiVariables.py
@@ -0,0 +1,10 @@
+i,j,k = 10, 20, 30
+
+print(i)
+print(j)
+print(k)
+
+m = n = o = 40
+print(m)
+print(n)
+print(o)
---
+++ variables.py
@@ -0,0 +1,8 @@
+i = 10;
+j = 20.2
+k = "Prince"
+
+print(i)
+print(j)
+print(k)
+
